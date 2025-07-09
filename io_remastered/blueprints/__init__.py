1cde6043bd125ae66d7fe42faf249e2764415a49
zNitche <53448802+zNitche@users.noreply.github.com>
2025-07-09 22:46:22 +0200

    feat: closing database session on app context teardown

--- io_remastered/blueprints/__init__.py
+++ io_remastered/blueprints/__init__.py
@@ -1,1 +1,9 @@
+from flask import current_app
+from io_remastered import db
+
 from io_remastered.blueprints.core.routes import core
+
+
+@current_app.teardown_appcontext
+def teardown_appcontext(exception=None):
+    db.close_session(exception=exception)
