42068e66e16482b4d4fa5d65e28e79a9d9f9b709
zNitche <53448802+zNitche@users.noreply.github.com>
2025-07-10 19:46:51 +0200

    refactor: database wrapper - replaced sqlalchemy Column with mapped_column

--- io_remastered/models.py
+++ io_remastered/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Integer, String
 from sqlalchemy.orm import mapped_column
 from io_remastered.db import Base
 
@@ -7,5 +7,5 @@ class User(Base):
     __tablename__ = "users"
 
     id = mapped_column(Integer, primary_key=True)
-    username = Column(String(20), unique=True, nullable=False)
-    password = Column(String(128), unique=False, nullable=False)
+    username = mapped_column(String(20), unique=True, nullable=False)
+    password = mapped_column(String(128), unique=False, nullable=False)
