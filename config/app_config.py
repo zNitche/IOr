8477acc989a2bf627d0a6f5a60c8ef22a8fd23b5
zNitche <53448802+zNitche@users.noreply.github.com>
2025-07-10 19:45:13 +0200

    refactor: app core config improvements

--- config/app_config.py
+++ config/app_config.py
@@ -3,12 +3,15 @@ from config import PROJECT_ROOT
 
 
 class AppConfig:
-    DEBUG_MODE = os.getenv("DEBUG", 0)
+    DEBUG_MODE = bool(int(os.getenv("DEBUG", 0)))
 
     APP_HOST = "0.0.0.0"
     APP_PORT = 8080
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    DATABASE_URI = f"sqlite:////{PROJECT_ROOT}/database/app.db"
+    DATABASE_ROOT_PATH = os.path.join(PROJECT_ROOT, "database")
+    MIGRATIONS_DIR_PATH = os.path.join(DATABASE_ROOT_PATH, "migrations")
+
+    DATABASE_URI = f"sqlite:////{DATABASE_ROOT_PATH}/app.db"
     LOGS_DIR_PATH = os.path.join(PROJECT_ROOT, "logs")
