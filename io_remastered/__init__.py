fe16ae58ef75cc1eb9a1764a5cb0291d2a571355
zNitche <53448802+zNitche@users.noreply.github.com>
2025-07-12 14:07:06 +0200

    feat: CSRF module base app in tegration

--- io_remastered/__init__.py
+++ io_remastered/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
-import os
+import secrets
 from config.app_config import AppConfig
 from io_remastered.db import Database
 from io_remastered.logging import Logging
+from io_remastered.csrf_protection import CSRF
 
 
 __version__ = "0.0.1"
@@ -17,16 +18,7 @@ def register_blueprints(app: Flask):
     app.register_blueprint(blueprints.core)
 
 
-def create_app(config_class: type[AppConfig]):
-    app = Flask(__name__, instance_relative_config=False)
-
-    app.secret_key = os.urandom(
-        30) if not config_class.DEBUG_MODE else "debug_secret"
-    app.config.from_object(config_class)
-
-    db.setup(app.config["DATABASE_URI"])
-    db.create_all()
-
+def setup_app_modules(app: Flask):
     app_logging = Logging(
         app=app,
         logs_filename="app.log",
@@ -34,6 +26,22 @@ def create_app(config_class: type[AppConfig]):
         backup_log_files_count=3)
 
     app_logging.setup()
+
+    db.setup(app.config["DATABASE_URI"])
+    db.create_all()
+
+    csrf = CSRF(app)
+    csrf.setup()
+
+
+def create_app(config_class: type[AppConfig]):
+    app = Flask(__name__, instance_relative_config=False)
+
+    app.secret_key = secrets.token_hex(
+        nbytes=32) if not config_class.DEBUG_MODE else "debug_secret"
+    app.config.from_object(config_class)
+
+    setup_app_modules(app)
 
     with app.app_context():
         register_blueprints(app)
