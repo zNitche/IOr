f8ab1a436a43a0c232e4158644893220fa291570
zNitche <53448802+zNitche@users.noreply.github.com>
2025-07-15 23:00:49 +0200

    feat: proper blueprints exports

--- io_remastered/blueprints/__init__.py
+++ io_remastered/blueprints/__init__.py
@@ -2,6 +2,8 @@ from flask import current_app
 from io_remastered import db
 
 from io_remastered.blueprints.core.routes import core
+from io_remastered.blueprints.auth.routes import auth
+from io_remastered.blueprints.errors.routes import errors
 
 
 @current_app.teardown_appcontext
