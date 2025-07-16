d859b3b72fff4b1fde52556624921680bdc0af8c
zNitche <53448802+zNitche@users.noreply.github.com>
2025-07-16 21:20:26 +0200

    feat: disabled loading .env files by flask

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ if __name__ == "__main__":
     APP_HOST = app.config["APP_HOST"]
     DEBUG_MODE = app.config["DEBUG_MODE"]
 
-    app.run(debug=DEBUG_MODE, host=APP_HOST, port=APP_PORT, threaded=True)
+    app.run(debug=DEBUG_MODE, host=APP_HOST, port=APP_PORT,
+            threaded=True, load_dotenv=False)
