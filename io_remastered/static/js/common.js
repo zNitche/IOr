e08e65652279737873d449a29112094467948a66
zNitche <53448802+zNitche@users.noreply.github.com>
2025-07-18 22:39:33 +0200

    feat: disable login button on form submit

--- io_remastered/static/js/common.js
+++ io_remastered/static/js/common.js
@@ -1,4 +1,4 @@
-async function closeFlashMessage(event) {
+function closeFlashMessage(event) {
     const container = event.target.parentElement;
 
     if (container) {
@@ -7,3 +7,13 @@ async function closeFlashMessage(event) {
         console.error("flash message container not found")
     }
 }
+
+function disableButtonById(buttonId) {
+    const button = document.getElementById(buttonId);
+
+
+    if (button) {
+        button.disabled = true;
+    }
+}
+
