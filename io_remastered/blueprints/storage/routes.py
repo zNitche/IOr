423792652dec615dc111cba11a0271cfc0c5af9a
zNitche <53448802+zNitche@users.noreply.github.com>
2025-07-20 21:02:32 +0200

    feat: file upload page - styling improvements

--- io_remastered/blueprints/storage/routes.py
+++ io_remastered/blueprints/storage/routes.py
@@ -1,12 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Response
 from io_remastered.authentication.decorators import login_required
+from io_remastered.io_csrf.decorators import csrf_protected
 
 
 storage = Blueprint("storage", __name__, template_folder="templates",
                  static_folder="static", url_prefix="/files")
 
 
-@storage.route("/upload")
+@storage.route("/upload", methods=["GET"])
 @login_required
-def file_upload():
+def upload():
     return render_template("upload.html")
+
+
+@storage.route("/upload/submit", methods=["POST"])
+@login_required
+@csrf_protected()
+def upload_handler():
+    return Response(), 200
--- io_remastered/static/js/file_upload.js
+++ io_remastered/static/js/file_upload.js
@@ -1,20 +1,92 @@
-function handleUploadFileSelected(event) {
-    const filesInput = event.currentTarget;
+function setupFileUploadPage() {
+    toggleElementVisibility("file-upload-error", false);
+    toggleElementVisibility("file-upload-button", false);
+    toggleElementVisibility("file-upload-progress-bar", false);
+    toggleElementVisibility("file-upload-name", false);
+}
 
-    if (filesInput.files.length === 1) {
+function getInputFile() {
+    const filesInput = document.getElementById("file-upload-input");
+
+    if (filesInput.files.length > 0) {
         const file = filesInput.files[0];
-        const fileName = file.name;
-        const fileSize = file.size;
+        return file
+    } else {
+        return null;
+    }
+}
 
-        showUploadFileElements(fileName);
+function handleUploadFileSelected() {
+    toggleElementVisibility("file-upload-error", false);
+
+    const file = getInputFile();
+
+    if (file) {
+        showUploadFileElements(file.name);
+        toggleElementVisibility("file-upload-button", true);
     }
 }
 
 function showUploadFileElements(filename) {
-    const filenameElement = document.getElementById("file-upload-name");
-    filenameElement.style.display = "block";
-    filenameElement.innerHTML = filename;
+    toggleElementVisibility("file-upload-name", true);
 
-    const uploadButton = document.getElementById("file-upload-button");
-    uploadButton.style.display = "block";
+    const filenameElement = document.getElementById("file-upload-name");
+    filenameElement.innerHTML = filename;
+}
+
+function showErrorMessage(message) {
+    toggleElementVisibility("file-upload-error", true);
+
+    const errorWrapper = document.getElementById("file-upload-error");
+    errorWrapper.innerHTML = `upload has failed : ${message}!`;
+}
+
+function sendFile(upload_url, csrf_token) {
+    const file = getInputFile();
+
+    if (!file) {
+        showErrorMessage("can't access selected file")
+        return;
+    }
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.upload.addEventListener("abort", (event) => handleSendFileError(event))
+    xhr.upload.addEventListener("error", (event) => handleSendFileError(event))
+    xhr.upload.addEventListener("timeout", (event) => handleSendFileError(event))
+
+    xhr.upload.addEventListener("load", (event) => handleSendFileLoad(event))
+    xhr.upload.addEventListener("loadend", (event) => handleSendFileLoadEnd(event))
+    xhr.upload.addEventListener("loadstart", (event) => handleSendFileLoadStart(event))
+
+    xhr.upload.addEventListener("progress", (event) => handleSendFileLoadProgress(event))
+
+    xhr.open("POST", upload_url, true);
+
+    xhr.setRequestHeader("X-File-Name", file.name);
+    xhr.setRequestHeader("X-File-Size", file.size);
+    xhr.setRequestHeader("Content-Type", file.type);
+    xhr.setRequestHeader("X-CSRF-TOKEN", csrf_token);
+
+    xhr.send();
+}
+
+function handleSendFileError(event) {
+    showErrorMessage(event.type)
+}
+
+function handleSendFileLoad(event) {
+
+}
+
+function handleSendFileLoadEnd(event) {
+
+}
+
+function handleSendFileLoadStart(event) {
+
+}
+
+function handleSendFileLoadProgress(event) {
+
 }
