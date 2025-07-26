8572d9f8fd33ac74e7736c73488b3fb8974551b1
zNitche <53448802+zNitche@users.noreply.github.com>
2025-07-26 20:34:54 +0200

    fix: app.py removed unused config entries

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ app = create_app(config_class=AppConfig)
 
 
 if __name__ == "__main__":
-    APP_PORT = app.config["APP_PORT"]
-    APP_HOST = app.config["APP_HOST"]
     DEBUG_MODE = app.config["DEBUG"]
 
-    app.run(debug=DEBUG_MODE, host=APP_HOST, port=APP_PORT,
+    app.run(debug=DEBUG_MODE, host="0.0.0.0", port=8080,
             threaded=True, load_dotenv=False)
