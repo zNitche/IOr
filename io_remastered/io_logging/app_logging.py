0271998d9b7f96cafd28e98b4493968b32941165
zNitche <53448802+zNitche@users.noreply.github.com>
2025-08-17 23:19:29 +0200

    refactor: common loggers logic extracted to separated file

--- io_remastered/io_logging/app_logging.py
+++ io_remastered/io_logging/app_logging.py
@@ -1,8 +1,6 @@
-import logging
-import os
 from flask import Flask
 from flask.logging import create_logger
-from logging.handlers import TimedRotatingFileHandler
+from io_remastered.io_logging import common
 
 
 class AppLogging:
@@ -16,58 +14,12 @@ class AppLogging:
 
         self.backup_log_files_count = backup_log_files_count
 
-        self.logs_path = self.__set_logs_path(logs_filename, logs_path)
+        self.logs_path = common.set_logs_path(logs_filename, logs_path)
         self.__logger = create_logger(app)
 
     def setup(self):
         # clear built in logging handlers
         self.__logger.handlers.clear()
 
-        if self.app.debug:
-            self.__logger.setLevel("DEBUG")
-        else:
-            self.__logger.setLevel("INFO")
-
-        self.__setup_serial()
-
-        if self.logs_path is not None:
-            self.__setup_file(self.logs_path)
-
-    def __set_logs_path(self, filename: str | None, path: str | None) -> str | None:
-        if filename is None or path is None:
-            return None
-
-        if not os.path.exists(path):
-            os.makedirs(path, exist_ok=True)
-
-        return os.path.join(path, filename)
-
-    def __setup_serial(self):
-        formatter = self.__get_formatter(with_day=False)
-
-        console_logger = logging.StreamHandler()
-        console_logger.setFormatter(formatter)
-
-        self.__logger.addHandler(console_logger)
-
-    def __setup_file(self, path: str):
-        formatter = self.__get_formatter()
-
-        file_handler = TimedRotatingFileHandler(filename=path,
-                                                when="midnight",
-                                                encoding="utf-8",
-                                                backupCount=self.backup_log_files_count)
-        file_handler.setFormatter(formatter)
-
-        self.__logger.addHandler(file_handler)
-
-    def __get_formatter(self, with_day: bool = True) -> logging.Formatter:
-        format = "%Y-%m-%d %H:%M:%S" if with_day else "%H:%M:%S"
-
-        formatter = logging.Formatter(
-            "{asctime} - {name} - {levelname} - {message}",
-            style="{",
-            datefmt=format,
-        )
-
-        return formatter
+        common.setup(logger=self.__logger, logs_path=self.logs_path,
+                     is_debug=self.app.debug, backup_log_files_count=self.backup_log_files_count)
--- io_remastered/io_logging/logger.py
+++ io_remastered/io_logging/logger.py
@@ -1,7 +1,7 @@
 from typing import Any
 import os
 import logging
-from logging.handlers import TimedRotatingFileHandler
+from io_remastered.io_logging import common
 from io_remastered.io_logging.adapters.thread_logger_adapter import ThreadLoggerAdapter
 
 
@@ -30,7 +30,8 @@ class Logger:
 
         self.log_to_file = log_to_file
         self.backup_log_files_count = backup_log_files_count
-        self.logs_path = self.__set_logs_path(logs_filename, logs_path)
+        self.logs_path = common.set_logs_path(
+            filename=logs_filename, path=logs_path)
 
         self.__setup()
 
@@ -38,52 +39,8 @@ class Logger:
         if self.__logger is None:
             raise Exception("can't setup None logger")
 
-        self.__logger.setLevel(
-            "DEBUG" if self.debug_mode else "INFO")
-        self.__setup_serial()
-
-        if self.logs_path is not None and self.log_to_file:
-            self.__setup_file(logs_path=self.logs_path,
-                              backup_log_files_count=self.backup_log_files_count)
-
-    def __setup_serial(self):
-        formatter = self.__get_formatter(with_day=False)
-
-        console_logger = logging.StreamHandler()
-        console_logger.setFormatter(formatter)
-
-        self.__logger.addHandler(console_logger)  # type: ignore
-
-    def __setup_file(self, logs_path: str, backup_log_files_count: int):
-        formatter = self.__get_formatter()
-
-        file_handler = TimedRotatingFileHandler(filename=logs_path,
-                                                when="midnight",
-                                                encoding="utf-8",
-                                                backupCount=backup_log_files_count)
-        file_handler.setFormatter(formatter)
-
-        self.__logger.addHandler(file_handler)  # type: ignore
-
-    def __set_logs_path(self, filename: str | None, path: str | None) -> str | None:
-        if filename is None or path is None:
-            return None
-
-        if not os.path.exists(path):
-            os.makedirs(path, exist_ok=True)
-
-        return os.path.join(path, filename)
-
-    def __get_formatter(self, with_day: bool = True) -> logging.Formatter:
-        format = "%Y-%m-%d %H:%M:%S" if with_day else "%H:%M:%S"
-
-        formatter = logging.Formatter(
-            "{asctime} - {levelname} - {name} - {message}",
-            style="{",
-            datefmt=format,
-        )
-
-        return formatter
+        common.setup(logger=self.__logger, logs_path=self.logs_path,
+                     is_debug=self.debug_mode, backup_log_files_count=self.backup_log_files_count)
 
     @staticmethod
     def for_thread(logger_name: str, thread_uid: str | int):
