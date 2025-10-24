c8b9d5f884ec08014755c336413b42f4b7b6b964
zNitche <53448802+zNitche@users.noreply.github.com>
2025-10-24 21:02:37 +0200

    refactor: moved add_directory route to storage blueprint

--- io_remastered/blueprints/core/routes.py
+++ io_remastered/blueprints/core/routes.py
@@ -1,13 +1,8 @@
-from flask import Blueprint, render_template, request, abort, redirect, flash
-from werkzeug.utils import secure_filename
+from flask import Blueprint, render_template, request, abort
 from io_remastered.authentication.decorators import login_required
-from io_remastered.consts import DirectoriesConsts
-from io_remastered.types import FlashTypeEnum
-from io_remastered.io_csrf import csrf_protected, CSRF
-from io_remastered import authentication_manager, models, forms, i18n, db
+from io_remastered.io_csrf import CSRF
+from io_remastered import authentication_manager, models, forms
 from io_remastered.db.pagination import Pagination, pageable_content
-from io_remastered.types import ActionLogKeyEnum
-from io_remastered.utils import system_logs_utils
 
 
 core = Blueprint("core", __name__, template_folder="templates",
@@ -97,48 +92,3 @@ def directories(page_id: int):
                            search_form=search_form,
                            create_directory_form=create_directory_form,
                            only_shared=only_shared)
-
-
-@core.route("/add-directory", methods=["POST"])
-@login_required
-@csrf_protected()
-def add_directory():
-    current_user = authentication_manager.current_user
-    form = forms.CreateDirectoryForm(form_data=request.form)
-
-    if form.is_valid():
-        dirname = form.get_field_value("name")
-        name = secure_filename(filename=dirname if dirname else "")
-
-        if name in DirectoriesConsts.FORBIDDEN_NAMES:
-            flash(i18n.t('create_directory_modal.forbidden_name'),
-                  FlashTypeEnum.Error.value)
-
-            return redirect(location=request.referrer)
-
-        directory_with_same_name_query = models.Directory.select().filter(
-            models.Directory.name == name, models.Directory.owner_id == current_user.id)
-        directory_with_same_name = models.Directory.query(
-            directory_with_same_name_query).first()
-
-        if directory_with_same_name is None:
-            directory = models.Directory(name=name, owner_id=current_user.id)
-            db.add(directory)
-
-            flash(i18n.t('create_directory_modal.directory_created'),
-                  FlashTypeEnum.Success.value)
-            
-            system_logs_utils.log_action(key=ActionLogKeyEnum.DirectoryCreated, metadata={
-                "name": directory.name,
-                "uuid": directory.uuid
-            })
-
-        else:
-            flash(i18n.t('create_directory_modal.directory_already_exists', format={"dir_name": name}),
-                  FlashTypeEnum.Error.value)
-
-    else:
-        flash(i18n.t('create_directory_modal.unexpeted_error'),
-              FlashTypeEnum.Error.value)
-
-    return redirect(location=request.referrer)
--- io_remastered/blueprints/storage/routes.py
+++ io_remastered/blueprints/storage/routes.py
@@ -1,6 +1,7 @@
 import os
 from flask import Blueprint, render_template, abort, send_file, current_app, \
     url_for, redirect, request, flash
+from werkzeug.utils import secure_filename
 from io_remastered.authentication.decorators import login_required
 from io_remastered.io_csrf.decorators import csrf_protected
 from io_remastered.consts import DirectoriesConsts
@@ -182,6 +183,51 @@ def directory_preview(page_id: int, uuid: str):
                            search_form=search_form,
                            files_pagination=files_pagination,
                            rename_directory_form=rename_directory_form)
+
+
+@storage.route("/add-directory", methods=["POST"])
+@login_required
+@csrf_protected()
+def add_directory():
+    current_user = authentication_manager.current_user
+    form = forms.CreateDirectoryForm(form_data=request.form)
+
+    if form.is_valid():
+        dirname = form.get_field_value("name")
+        name = secure_filename(filename=dirname if dirname else "")
+
+        if name in DirectoriesConsts.FORBIDDEN_NAMES:
+            flash(i18n.t('create_directory_modal.forbidden_name'),
+                  FlashTypeEnum.Error.value)
+
+            return redirect(location=request.referrer)
+
+        directory_with_same_name_query = models.Directory.select().filter(
+            models.Directory.name == name, models.Directory.owner_id == current_user.id)
+        directory_with_same_name = models.Directory.query(
+            directory_with_same_name_query).first()
+
+        if directory_with_same_name is None:
+            directory = models.Directory(name=name, owner_id=current_user.id)
+            db.add(directory)
+
+            flash(i18n.t('create_directory_modal.directory_created'),
+                  FlashTypeEnum.Success.value)
+            
+            system_logs_utils.log_action(key=ActionLogKeyEnum.DirectoryCreated, metadata={
+                "name": directory.name,
+                "uuid": directory.uuid
+            })
+
+        else:
+            flash(i18n.t('create_directory_modal.directory_already_exists', format={"dir_name": name}),
+                  FlashTypeEnum.Error.value)
+
+    else:
+        flash(i18n.t('create_directory_modal.unexpeted_error'),
+              FlashTypeEnum.Error.value)
+
+    return redirect(location=request.referrer)
 
 
 @storage.route("/directory/<uuid>/remove", methods=["POST"])
