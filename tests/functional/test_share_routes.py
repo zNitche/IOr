59b90e17abf13748cd958eda1097cd1748de43a6
zNitche <53448802+zNitche@users.noreply.github.com>
2025-10-26 23:32:26 +0100

    feat: tests - share routes tests

--- tests/functional/test_share_routes.py
+++ tests/functional/test_share_routes.py
@@ -1,15 +1,83 @@
 from flask import url_for
+from tests.consts import StorageConsts
 
 
-def test_home_as_non_auth(test_client):
-    response = test_client.get(url_for("core.home"), follow_redirects=True)
+def test_file_preview_missing_as_non_auth(test_client, with_shared_file):
+    file_url = url_for(
+        "share.file_preview", share_uuid="123")
+
+    response = test_client.get(file_url, follow_redirects=True)
+
+    assert response.status_code == 404
+    assert response.request.path == file_url
+
+
+def test_file_preview_as_non_auth(test_client):
+    file_url = url_for(
+        "share.file_preview", share_uuid=StorageConsts.SHARED_FILE_SHARE_UUID)
+
+    response = test_client.get(file_url, follow_redirects=True)
 
     assert response.status_code == 200
-    assert response.request.path == url_for("auth.login")
+    assert response.request.path == file_url
 
 
-def test_home_as_auth(test_client, logged_test_user):
-    response = test_client.get(url_for("core.home"), follow_redirects=True)
+def test_file_preview_as_auth(test_client, logged_test_user):
+    file_url = url_for(
+        "share.file_preview", share_uuid=StorageConsts.SHARED_FILE_SHARE_UUID)
+
+    response = test_client.get(file_url, follow_redirects=True)
 
     assert response.status_code == 200
-    assert response.request.path == url_for("core.home")
+    assert response.request.path == file_url
+
+
+def test_directory_preview_missing_as_non_auth(test_client):
+    file_url = url_for(
+        "share.directory_preview", share_uuid="123")
+
+    response = test_client.get(file_url, follow_redirects=True)
+
+    assert response.status_code == 404
+    assert response.request.path == file_url
+
+
+def test_directory_preview_as_non_auth(test_client, with_shared_directory):
+    file_url = url_for(
+        "share.directory_preview", share_uuid=StorageConsts.SHARED_DIRECTORY_SHARE_UUID)
+
+    response = test_client.get(file_url, follow_redirects=True)
+
+    assert response.status_code == 200
+    assert response.request.path == file_url
+
+
+def test_directory_preview_as_auth(test_client, logged_test_user):
+    file_url = url_for(
+        "share.directory_preview", share_uuid=StorageConsts.SHARED_DIRECTORY_SHARE_UUID)
+
+    response = test_client.get(file_url, follow_redirects=True)
+
+    assert response.status_code == 200
+    assert response.request.path == file_url
+
+
+def test_directory_download_as_non_auth(test_client):
+    file_url = url_for(
+        "share.download_directory", share_uuid=StorageConsts.SHARED_DIRECTORY_SHARE_UUID)
+
+    response = test_client.get(file_url, follow_redirects=True)
+
+    assert response.status_code == 200
+    assert response.request.path == file_url
+
+
+def test_file_download_as_non_auth(test_client):
+    file_url = url_for(
+        "share.download_file", share_uuid=StorageConsts.SHARED_FILE_SHARE_UUID)
+
+    try:
+        test_client.get(file_url, follow_redirects=True)
+    except FileNotFoundError:
+        pass
+
