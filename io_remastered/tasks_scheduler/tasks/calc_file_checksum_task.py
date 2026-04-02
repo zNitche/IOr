d96068c79401cbb96f2289703651e72a5a7353f0
zNitche <53448802+zNitche@users.noreply.github.com>
2026-04-02 19:50:01 +0200

    feat: tasks scheduler - taskslogging implementation

--- io_remastered/tasks_scheduler/tasks/calc_file_checksum_task.py
+++ io_remastered/tasks_scheduler/tasks/calc_file_checksum_task.py
@@ -13,19 +13,19 @@ class CalcFileChecksumTask(TaskBase):
         from io_remastered import models
 
         if db is None:
-            raise Exception("")
+            raise Exception("db is none")
 
         file_uuid = self.args.get("file_uuid")
         file_path = self.args.get("target_file_path")
 
         if not file_uuid or not file_path:
-            raise Exception("")
+            raise Exception("file_uuid and/or file_path is None")
 
         file = models.File.query(
             models.File.select().filter_by(uuid=file_uuid)).first()
 
         if not file:
-            raise Exception("")
+            raise Exception(f"file for uuid {file_uuid} has not been found")
 
         sha256sum = files_utils.get_sha256sum_for_file(file_path=file_path)
         file.sha256_sum = sha256sum
--- io_remastered/tasks_scheduler/tasks/task_base.py
+++ io_remastered/tasks_scheduler/tasks/task_base.py
@@ -1,8 +1,10 @@
+import time
+import os
 from typing import Any
 from collections.abc import Callable
 from threading import Thread
 from io_remastered.db import Database
-import time
+from io_remastered.io_logging import Logger
 
 
 class TaskBase:
@@ -12,6 +14,16 @@ class TaskBase:
 
         self.timestamp = str(time.time())
         self.__thread: Thread | None = None
+
+        self.logger = self.__get_logger()
+
+    def __get_logger(self):
+        return Logger.get_expandable_logger(
+            logger_name="ScheduledTask",
+            extra={"uuid": self.uuid},
+            logs_path=os.path.join("logs", "tasks_scheduler", "tasks"),
+            logs_filename="scheduled_task.log",
+            backup_log_files_count=1)
 
     @classmethod
     def get_name(cls):
@@ -25,11 +37,12 @@ class TaskBase:
     def __mainloop(self, db: Database | None, on_complete_callback: Callable[[str], None]):
         try:
             self._runner(db=db)
-        
+
         except:
-            pass
+            self.logger.exception(
+                f"exception while running {self.__class__.__name__} [{self.uuid}] , with args: {self.args}")
 
         on_complete_callback(self.uuid)
 
     def _runner(self, db: Database | None):
-         raise NotImplementedError()
+        raise NotImplementedError()
